cd3f62e5757eb77ecb87057109f66c05a5b70aff
tawnchan <tawnchan@gmail.com>
2022-09-26 15:37:16 -0700

    working on playerOne and playerTwo

--- src/App.js
+++ src/App.js
@@ -3,12 +3,42 @@ import Square from './components/Square'
 import './App.css'
 
 const App = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null))
+  const [square, setSquare] = useState(Array(9).fill(null))
+
+  const [playerOne, setPlayerOne] = useState(true)
+  const [playerTwo, setPlayerTwo] = useState(true)
+
+  const handleGamePlay = (clickSquare) => {
+    let newBoard = [...square]
+      console.log(playerOne, playerTwo, "test line 0")
+    if (playerTwo) {
+      newBoard[clickSquare] = false ? "X" : "O"
+      setSquare(newBoard)
+      setPlayerOne(playerOne = false)
+      setPlayerTwo(playerTwo = true)
+      console.log(playerOne, playerTwo, "test line 1")
+    } 
+    // else {
+    //   newBoard[clickSquare] = true ? "O" : "X"
+    // }
+  }
 
   return (
     <>
       <h1>Tic Tac Toe</h1>
-      <Square />
+      <div className='board'>
+
+        {square.map((square, index) => {
+
+          return <Square
+            square={square}
+            index={index}
+            handleGamePlay={handleGamePlay}
+            key={index}
+
+          />
+        })}
+      </div>
     </>
   )
 }
