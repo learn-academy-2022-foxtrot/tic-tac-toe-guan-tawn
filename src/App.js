06f70d580e9a85da827ae88707f2ee366e0366fe
guanhchan <guanh.chan@gmail.com>
2022-09-27 11:47:49 -0700

    Most logic completed

--- src/App.js
+++ src/App.js
@@ -7,16 +7,17 @@ const App = () => {
 
   let [playerOne, setPlayerOne] = useState(true)
   let [playerTwo, setPlayerTwo] = useState(false)
-  
-  let [squareStyle,setSquareStyle] = useState({
-    ifClicked : false
 
-  })
+  // let [squareStyle,setSquareStyle] = useState({
+  //   ifClicked : false
 
-  
+  // })
+
+  // let ifClicked = false
 
   const handleGamePlay = (clickSquare) => {
     let newBoard = [...square]
+     
     //console.log(playerOne, playerTwo, "test line 0")
     if (playerOne) {
       newBoard[clickSquare] = true ? "X" : "O"
@@ -25,10 +26,11 @@ const App = () => {
       setPlayerOne(playerOne)
       playerTwo = true
       setPlayerTwo(playerTwo)
-
+  
       //console.log(playerOne, playerTwo, "test line 1")
-    } 
-    else if (playerTwo){
+    }
+    else if (playerTwo) {
+      
       // console.log(playerOne, playerTwo,'play two')
       newBoard[clickSquare] = true ? "O" : "X"
       setSquare(newBoard)
@@ -36,19 +38,91 @@ const App = () => {
       setPlayerOne(playerOne)
       playerTwo = false
       setPlayerTwo(playerTwo)
+
     }
     else {
-     return 'error'
+      return 'error'
     }
+   handleWin()
+
+    let winCon = {
+      winCon1: [square[0], square[1], square[2]],
+      winCon2: [square[3], square[4], square[5]],
+      winCon3: [square[6], square[7], square[8]],
+      winCon4: [square[0], square[3], square[6]],
+      winCon5: [square[1], square[4], square[7]],
+      winCon6: [square[2], square[5], square[8]],
+      winCon7: [square[0], square[4], square[8]],
+      winCon8: [square[2], square[4], square[6]],
+    }
+
+    // let xWins = ['X', 'X', 'X']
+    // console.log(xWins)
+    // // let oWins = ['O','O','O']
+    
+    // console.log(winCon.winCon1 )
+    // console.log(winCon.winCon1 == xWins)
+    // if (winCon.winCon1 !== null){
+    //   alert ('You Won')
+    // }
 
     //console.log(squareStyle.ifClicked)
   }
 
 
+      const wins = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    let winCon = {
+      winCon1: [square[0], square[1], square[2]],
+      winCon2: [square[3], square[4], square[5]],
+      winCon3: [square[6], square[7], square[8]],
+      winCon4: [square[0], square[3], square[6]],
+      winCon5: [square[1], square[4], square[7]],
+      winCon6: [square[2], square[5], square[8]],
+      winCon7: [square[0], square[4], square[8]],
+      winCon8: [square[2], square[4], square[6]],
+    }
+
+  const handleWin = () => {
+
+    for (let i = 0; i < wins.length; i++) {
+      const [a, b, c] = wins[i];
+      if (
+        square[a] &&
+        square[a] === square[b] &&
+        square[a] === square[c]
+      ) {
+        console.log(square[a])
+        alert(`${square[a]}'s Wins`);
+        //handleReset()
+      }
+
+    }
+    return null
+  };
+
+  let handleReset = () => {
+    setSquare(Array(9).fill(null))
+    setPlayerOne(true)
+    setPlayerTwo(false)
+  }
+
 
   return (
     <>
       <h1>Tic Tac Toe</h1>
+      <button onClick={handleReset}>
+        Reset Game
+      </button>
       <div className='board'>
 
         {square.map((square, index) => {
@@ -58,11 +132,14 @@ const App = () => {
             index={index}
             handleGamePlay={handleGamePlay}
             key={index}
-            squareStyle = {squareStyle}
+          // squareStyle = {squareStyle}
+          // ifClicked ={ifClicked} 
 
 
           />
         })}
+
+        
       </div>
     </>
   )
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,15 +1,33 @@
 import React from 'react'
 
 const Square = (props) => {
-  console.log(props)
+
   const handleClick = () => {
-    props.handleGamePlay(props.index)
-    props.squareStyle.ifClicked = true
+    if (props.square === null) {
+      props.handleGamePlay(props.index)
+
+    } else {
+
+      let ranPhrase = ['Try Again Dummy',
+        "What are you doing?",
+        'Do you know even know how to play?',
+        'Bro, That Aint it',
+        'X s and O s !! X s && O s', 'She dont like you..',
+        "You are not going to win like that, play checkers"]
+        
+      alert(ranPhrase[Math.floor(Math.random() * ranPhrase.length)])
+    }
+
+    // props.squareStyle.ifClicked = true
+    console.log(props)
+
+
+
 
   }
 
   return (
-    <div className="square" onClick={handleClick} key={props.index}>
+    <div className="square" onClick={handleClick} key={props.index} >
       {props.square}
     </div>
   )
