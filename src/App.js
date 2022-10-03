7a73cb7071d0beb7969720e9b5ae2f457bec8b81
guanhchan <guanh.chan@gmail.com>
2022-10-03 11:36:35 -0700

    tic tac toe done

--- src/App.js
+++ src/App.js
@@ -7,13 +7,8 @@ const App = () => {
 
   let [playerOne, setPlayerOne] = useState(true)
   let [playerTwo, setPlayerTwo] = useState(false)
-
-  // let [squareStyle,setSquareStyle] = useState({
-  //   ifClicked : false
-
-  // })
-
-  // let ifClicked = false
+  let [playGame, setPlayGame] = useState(true)
+  let [message, setMessage] = useState("")
 
   const handleGamePlay = (clickSquare) => {
     let newBoard = [...square]
@@ -45,28 +40,6 @@ const App = () => {
     }
    handleWin()
 
-    let winCon = {
-      winCon1: [square[0], square[1], square[2]],
-      winCon2: [square[3], square[4], square[5]],
-      winCon3: [square[6], square[7], square[8]],
-      winCon4: [square[0], square[3], square[6]],
-      winCon5: [square[1], square[4], square[7]],
-      winCon6: [square[2], square[5], square[8]],
-      winCon7: [square[0], square[4], square[8]],
-      winCon8: [square[2], square[4], square[6]],
-    }
-
-    // let xWins = ['X', 'X', 'X']
-    // console.log(xWins)
-    // // let oWins = ['O','O','O']
-    
-    // console.log(winCon.winCon1 )
-    // console.log(winCon.winCon1 == xWins)
-    // if (winCon.winCon1 !== null){
-    //   alert ('You Won')
-    // }
-
-    //console.log(squareStyle.ifClicked)
   }
 
 
@@ -101,19 +74,32 @@ const App = () => {
         square[a] === square[b] &&
         square[a] === square[c]
       ) {
-        console.log(square[a])
-        alert(`${square[a]}'s Wins`);
-        //handleReset()
+        return square[a]
+        
       }
 
     }
     return null
   };
+  if(playGame){
+
+    if(handleWin(square) === "X"){
+      setMessage(<h1> Player One Wins X</h1>)
+      setPlayGame(false)
+    } else if(handleWin(square) === "O"){
+      setMessage(<h1> Player Two Wins O</h1>)
+    } else if(square.indexOf(null) === -1){
+      setMessage(<h1> Draw, play again</h1>)
+      setPlayGame(false)
+    }
+  }
 
   let handleReset = () => {
     setSquare(Array(9).fill(null))
     setPlayerOne(true)
     setPlayerTwo(false)
+    setPlayGame(true)
+    setMessage("")
   }
 
 
@@ -123,8 +109,9 @@ const App = () => {
       <button onClick={handleReset}>
         Reset Game
       </button>
+       {message}
       <div className='board'>
-
+       
         {square.map((square, index) => {
 
           return <Square
